8faf13dee416eceba466fade4945c2cf3fe56775
zcotter@forwardfinancing.com <jaffecotter.z@gmail.com>
2017-05-01 15:16:32 -0400

    Exceptions should free process lock

--- test/lib/with_process_lock_test.rb
+++ test/lib/with_process_lock_test.rb
@@ -13,11 +13,28 @@ class ProcessLockTest < MiniTest::Test
   def test_redis_key_unavailable
     redis = Redis.new
     redis.set('sad_test', true)
-    assert_raises StandardError do
-      start = 1
-      ProcessLock::WithProcessLock.execute('sad_test') do
-        start += 1
+    assert_equal(
+      assert_raises(StandardError) do
+        start = 1
+        ProcessLock::WithProcessLock.execute('sad_test') do
+          start += 1
+        end
+      end.message,
+      'Can not run process because sad_test taken.'
+    )
+  end
+
+  class FakeException < RuntimeError
+  end
+
+  def test_block_exception_frees_key
+    begin
+      ProcessLock::WithProcessLock.execute('raises_exception') do
+        raise FakeException
       end
+    rescue FakeException => e
+      assert(e)
     end
+    refute Redis.new.get('raises_exception')
   end
 end
